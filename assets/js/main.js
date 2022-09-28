3ec69d55d260fa8151fdc957b1952b01b09ed176
Juliana da Cruz <juliana.da.cruz@avanade.com>
2022-09-28 16:20:38 -0300

    Desafio mudar a cor do background fim da rolagem

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Trabalhando com JavaScript em um arquivo externo.
+
+
+console.log(nome, "chamando variável criado com  var");
+
+var nome = "Fiap";
+
+escreve("exemplo de função nominal");
+
+//função nominal - udf
+function escreve(titulo){
+    alert(titulo);
+}
+
